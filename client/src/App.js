7b8c21fbe853dcca86bc6ed391b0d6d567b06870
Jonathan Kuhn <jkuhn96@gmail.com>
2022-11-07 00:10:05 -0500

    Added update person feature to FE

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Main from './views/Main'
-import Detail from './components/Detail'
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Main from "./views/Main";
+import Detail from "./components/Detail";
+import Update from "./components/Update";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route element={<Navigate to='/people'/>} path='/' />
-          <Route element={<Main/>} path='/people' />
-          <Route element={<Detail/>} path='/people/:id' />
+          <Route element={<Navigate to="/people" />} path="/" />
+          <Route element={<Main />} path="/people" />
+          <Route element={<Detail />} path="/people/:id" />
+          <Route element={<Update />} path="/people/edit/:id" />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/components/PersonList.js
+++ client/src/components/PersonList.js
@@ -1,36 +1,37 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const PersonList = (props) => {
-    const {people, setPeople} = props
+  const { people, setPeople } = props;
 
-    useEffect(() => {
-        axios.get("http://localhost:8000/api/people")
-            .then((res) => {
-                console.log(res.data)
-                setPeople(res.data)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }, [setPeople])
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/people")
+      .then((res) => {
+        console.log(res.data);
+        setPeople(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [setPeople]);
 
-    return (
-        <div>
-            {
-                people.map((person, index) => {
-                    return (
-                        <div key={index}>
-                            <p>{person.lastName}</p>
-                            <p>{person.firstName}</p>
-                            <Link to={`/people/${person._id}`}>{person.firstName}'s Page!</Link>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    )
-}
+  return (
+    <div>
+      {people.map((person, index) => {
+        return (
+          <div key={index}>
+            <p>{person.lastName}</p>
+            <p>{person.firstName}</p>
+            <Link to={`/people/${person._id}`}>{person.firstName}'s Page!</Link>
+            <span> | </span>
+            <Link to={`/people/edit/${person.id}`}>Edit</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default PersonList
+export default PersonList;
