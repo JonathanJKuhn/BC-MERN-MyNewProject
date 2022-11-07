b9108488abab1f3ebd77834b80c10bd95318d839
Jonathan Kuhn <jkuhn96@gmail.com>
2022-11-06 23:40:14 -0500

    Added updatePerson feature to API

--- server/controllers/person.controller.js
+++ server/controllers/person.controller.js
@@ -1,30 +1,36 @@
-const Person = require('../models/person.model')
+const Person = require("../models/person.model");
 module.exports.index = (req, res) => {
-    res.json({
-        message: "Hello World"
-    })
-}
+  res.json({
+    message: "Hello World",
+  });
+};
 
 module.exports.createPerson = (req, res) => {
-    Person.create(req.body)
-        .then(person => res.json(person))
-        .catch(err => res.json(err))
-}
+  Person.create(req.body)
+    .then((person) => res.json(person))
+    .catch((err) => res.json(err));
+};
 
 module.exports.getAllPeople = (req, res) => {
-    Person.find({})
-        .then(persons => {
-            console.log(persons)
-            res.json(persons)
-        })
-        .catch(err => {
-            console.log(err)
-            res.json(err)
-        })
-}
+  Person.find({})
+    .then((persons) => {
+      console.log(persons);
+      res.json(persons);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+};
 
 module.exports.getPerson = (req, res) => {
-    Person.findOne({_id:req.params.id})
-        .then(person => res.json(person))
-        .catch(err => res.json(err))
-}
+  Person.findOne({ _id: req.params.id })
+    .then((person) => res.json(person))
+    .catch((err) => res.json(err));
+};
+
+module.exports.updatePerson = (req, res) => {
+  Person.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then((updatedPerson) => res.json(updatedPerson))
+    .catch((err) => res.json(err));
+};
--- server/routes/person.routes.js
+++ server/routes/person.routes.js
@@ -1,7 +1,8 @@
-const PersonController = require('../controllers/person.controller')
+const PersonController = require("../controllers/person.controller");
 module.exports = (app) => {
-    app.get('/api', PersonController.index)
-    app.get('/api/people', PersonController.getAllPeople)
-    app.get('/api/people/:id', PersonController.getPerson)
-    app.post('/api/people', PersonController.createPerson)
-}
+  app.get("/api", PersonController.index);
+  app.get("/api/people", PersonController.getAllPeople);
+  app.get("/api/people/:id", PersonController.getPerson);
+  app.post("/api/people", PersonController.createPerson);
+  app.put("/api/people/:id", PersonController.updatePerson);
+};
